246aba9ce177bc76c4c805623f0dff9078344958
Adrian Lopez <adrianlzt@gmail.com>
2025-07-10 09:14:32 +0200

    refactor: use super parameters in settings_screen.dart

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -4,7 +4,7 @@ import '../services/llm_service.dart';
 import '../services/secure_storage_service.dart';
 
 class SettingsScreen extends StatefulWidget {
-  const SettingsScreen({Key? key}) : super(key: key);
+  const SettingsScreen({super.key});
 
   @override
   State<SettingsScreen> createState() => _SettingsScreenState();
